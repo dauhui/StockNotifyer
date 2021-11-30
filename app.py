37f8da1f65e6181de6b76672815a402c97643083
dauhui <rexyhyang@gmail.com>
2021-12-01 00:05:44 +0800

    fix: reply message

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ line_bot_api = LineBotApi('WP4eEnzMrN9wtkwGBV8o373h8UqDZgklWU2rFBP5gTf2CDZU6nB/H
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('1d0fdb14886436296be96a74215380e4')
 
-line_bot_api.push_message('U63fb6a74c963363406c0533e5f216958', TextSendMessage(text='你可以開始了'))
+line_bot_api.push_message('U63fb6a74c963363406c0533e5f216958', TextSendMessage(text='部屬成功'))
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -41,8 +41,8 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    message =event.message.text
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(message))
 
 #主程式
 import os
